788ae66d6c7b97a8d3a6a5134947676948860ed2
juanjbsalas <jbsalas05@gmail.com>
2025-06-26 15:30:00 -0400

    Fixed adding tasks to list

---
+++ Week 4/app.js
@@ -0,0 +1,41 @@
+// console.log(document.querySelector('#todos'));
+// console.log(document.querySelector('.todo'));
+
+
+// console.log("The script is running!");
+
+let todo1 = document.querySelector('#todos .todo');
+
+todo1.style.color = 'lightgrey';
+todo1.style.textDecoration = 'line-through';
+
+// Stepby-Step
+// 1: Get text input from textbox, function that gets input from textbox and returns input
+// 2: Send the previous return value to the function that once the addtask button is clicked,
+//    it is added on the li list.
+
+function getUserInput() {
+    console.log("Function 1 is running!!");
+    const userInput = document.getElementById('userInput');
+    const userInputValue = userInput.value;
+    
+    return userInputValue;
+}
+
+function addTask(x) { 
+    // x should be the userInputValue such as "Wash Laundry"
+    console.log("Now Figure 2 is running!");
+    document.getElementById('todos-list').innerHTML += `<li class="todo"><button>X</button>${x}</li>`;
+}
+
+
+// function addTask1() {
+//     console.log("This function is running!")
+// }
+
+
+
+
+
+
+
